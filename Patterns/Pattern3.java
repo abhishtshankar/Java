53fd51eaa85c6da0b852f35ddadd090dc5e64531
Rituraj Gupta <rituraj.iter@gmail.com>
2020-10-07 21:01:08 +0530

    Add files via upload

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,20 @@
+/*
+    1
+   101
+  10101
+ 1010101
+101010101
+*/
+public class Pattern3 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(j%2);
+            }
+            System.out.println();
+        }
+    }
+}
